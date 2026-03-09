65c95bed5a9c611f6abf9443acf9bb39b90271ee
eduardpolakov121-glitch <eduardpolakov121@gmail.com>
2026-03-09 17:32:22 +0200

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.KIDS_ROOM_SUPABASE_CONFIG = {
+  url: "https://xhhzxiithajxgngmbzzd.supabase.co",
+  anonKey: "sb_publishable_cRp6r2C_3nszludByS9V9Q_sl1QlHg5",
+  bucket: "product-images"
+};
